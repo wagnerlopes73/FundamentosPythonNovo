541ad0f397106bbe166fd497c249921b4081bdca
wagnerlopes73 <51516628+wagnerlopes73@users.noreply.github.com>
2023-01-29 22:52:09 -0300

    ok

---
+++ Cap4/DSA-Python-Cap04-01-Numeros.py
@@ -0,0 +1,57 @@
+# Soma
+var = 4 + 4
+print(var)
+
+# Subtração
+var = 4 - 3
+print(var)
+
+# Multiplicação
+var = 4 * 3
+print(var)
+
+# Divisão
+var = 4 / 3
+print(var)
+var2 = int(var)
+print(var2)
+
+# Potência
+var = 2 ** 3
+print(var)
+
+# Módulo
+var = 10 % 3
+print(var)
+
+# Divisão com resultado float
+var = 4 / 2
+print(var)
+
+# Divisão com resultado int
+var = 4 // 2
+print(var)
+
+# Divisão por float sempre resultará em float, mesmo com //
+var = 4 // 3.0
+print(var)
+
+# Transforma int para float
+var = float(9)
+print(var)
+
+# Trasnforma float para int
+var = int(6.5)
+print(var)
+
+# Retorna valor absoluto
+var = abs(-8)
+print(var)
+
+# Retorna valor com duas casas decimais
+var = round(3.1455465, 2)
+print(var)
+
+# Potência
+var = pow(4, 2)
+print(var)
