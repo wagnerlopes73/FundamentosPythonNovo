359a68dc8f6f7f6bff359f67e79abf30eb507f4e
wagnerlopes73 <51516628+wagnerlopes73@users.noreply.github.com>
2023-02-04 14:57:55 -0300

    ok

---
+++ Cap4/DSA-Python-Cap04-02-Variaveis.py
@@ -0,0 +1,36 @@
+# atribuindo valor a uma variável
+var_teste = 1
+print(var_teste)
+
+# verificar o tipo de variável
+var_teste2=type(var_teste)
+print(var_teste2)
+
+# Declaração Múltipla
+pessoa1, pessoa2, pessoa3 = "Wagner", "João", "Beth"
+print(pessoa1)
+print(pessoa2)
+print(pessoa3)
+
+fruta1 = fruta2 = fruta3 = "Morango"
+print(fruta1)
+print(fruta2)
+print(fruta3)
+
+# Calculo área
+largura = 4
+altura = 5
+area = largura * altura
+print(area)
+
+# Operações com variáveis
+idade1 = 25
+idade2 = 35
+soma1 = idade1 + idade2
+print(soma1)
+
+# Concatenação de Variáveis
+nome = "Wagner"
+sobrenome = "Lopes"
+fullname = nome + " " + sobrenome
+print(fullname)
---
+++ Cap4/DSA-Python-Cap04-03-Strings.py
@@ -0,0 +1,69 @@
+# Testando uma string
+a = "Wagner Lopes"
+print(a)
+
+# Testando uma string
+a = "João Carlos \nAna Clara \nWagner Lopes"
+print(a)
+
+s = "Data Science Academy"
+print(s)
+
+# Indexação - Apenas o primeiro caractere da string
+# Em Python a idenxação começa com 0
+s1 = s[0]
+print(s1)
+
+# Indexação - Apenas o primeiro caractere da string
+# Em Python a idenxação começa com 0
+s2 = s[1]
+print(s2)
+
+# Tudo a partir do índice 1
+s3 = s[1:]
+print(s3)
+
+# Entre os índices 6 e 9
+# slicing
+# valor depois do ":" é EXCLUSIVO, ou seja, não entra no retorno
+s4 = s[6:9]
+print(s4)
+
+# também podemos indexar de trás para frente
+s5 = s[-1]
+print(s5)
+
+# retorna tudo menos a última letra
+s6 = s[:-1]
+print(s6)
+
+# fatiar usando indexação saltando de 2 em 2
+s7 = s[::2]
+print(s7)
+
+# podemos ainda manipular de trás para frente
+s8 = s[::-1]
+print(s8)
+
+# criar repetição
+letra1 = "w"
+letra2 = letra1 * 3
+print(letra2)
+
+# Funções BUILT-IN de Strings
+
+# Upper Case
+s9 = s.upper()
+print(s9)
+
+# Lower Case
+s10 = s.lower()
+print(s10)
+
+# Primeira letra em Maiúsculo
+s11 = s.capitalize()
+print(s11)
+
+# Primeira letra maiúscula de cada palavra
+s12 = s.title()
+print(s12)
