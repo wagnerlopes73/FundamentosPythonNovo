8f2e02d7ebd08f5162920063311971ebed907742
wagnerlopes73 <51516628+wagnerlopes73@users.noreply.github.com>
2023-02-21 11:07:46 -0300

    ok

---
+++ Cap4/DSA-Python-Cap04-04-Listas.py
@@ -0,0 +1,20 @@
+# Criando Listas
+lista1 = ["Wagner", "Lopes", "João"]
+lista2 = [ 100, 10, "João"]
+lista3 = ["Wagner", 16, "Mãe"]
+
+# Atribuindo um valor da lista a uma variável
+item1 = lista1[1]
+item2 = lista2[2]
+
+# Atualizando um item da lista
+lista3[0]="Mônica"
+
+# deletando um item da lista
+del lista3[1]
+
+
+# LISTAS ANINHADAS
+# Listas de listas são matrizes no Python
+
+lista4 = [[1, 2, 3], [4, 5, 6], [7.1, 7.2, 7.3]]
